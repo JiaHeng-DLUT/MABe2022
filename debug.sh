8a5f3ccde728bfe718765bfc9b7c2210f9c23d94
jiaheng <jiaheng.dlut@gmail.com>
2022-09-10 05:08:38 +0800

    Add debug.sh train.sh

---
+++ debug.sh
@@ -0,0 +1,7 @@
+export PYTHONPATH=$PYTHONPATH:.
+CUDA_VISIBLE_DEVICES=0 \
+    python3 -m torch.distributed.launch \
+    --nproc_per_node=1 \
+    --master_port=4321 \
+    mabe/train.py \
+    -opt options/video/seed_0.yml
