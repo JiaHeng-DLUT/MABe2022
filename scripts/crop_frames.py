d06e7fa0e2559e631aa1dc785c597109f5f2843b
jiaheng <jiaheng.dlut@gmail.com>
2022-09-10 05:17:13 +0800

    Update scripts/crop_frames.py

--- scripts/crop_frames.py
+++ scripts/crop_frames.py
@@ -87,9 +87,7 @@ pool.join()
 print("Done")
 
 
-frame_dir = f"/cache"
 names = os.listdir(frame_dir)
 for name in tqdm(names):
     path = f"{frame_dir}/{name}"
-    frames = np.load(path)
-    assert frames.shape[0] == 1800, path
+    assert len(os.listdir(path)) == 1800, path
